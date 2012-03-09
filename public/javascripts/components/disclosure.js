36d820e6019e8b818cc99abf6db3784de8fc2c3f
inspector-cochise <immanuel.sims@akquinet.de>
2012-03-09 14:08:41 +0100

    better arrows

--- public/javascripts/components/disclosure.js
+++ public/javascripts/components/disclosure.js
@@ -3,7 +3,7 @@
  */
 var disclosure = function(){
 
-    var closedArrow = '\u25b6', openArrow = '\u25bc';
+    var closedArrow = '\u25b8', openArrow = '\u25be';
     
     /**
      * My own component object
